998c61858e1a0f35cc70ad687ef17b576ced5b66
Tarek Nabil <tareknabil698@gmail.com>
2024-09-04 19:44:44 +0300

    add-last

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,13 +1,7 @@
 const allowedOrigins = require("./allowedOrigins")
 
 const corsOptions={
-    origin:(origin,callback)=>{
-        if(allowedOrigins.indexOf(origin)!==-1 || !origin){
-            callback(null,true)
-        }  else{
-            callback(new Error("Not Allowed by CORS"))
-        } 
-    },
+    origin:true,
     //Any data I send with request accept it
     Credential:true,
     optionSuccessStatus:200,
